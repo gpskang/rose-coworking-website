83c4b35f1bd68181bdb08807d733bc11ee984a81
sushil <flemingoui@gmail.com>
2025-10-02 11:13:39 +0530

    first

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className=" flex flex-col items-center justify-center h-screens">
+    <div className=" flex flex-col items-center justify-center h-screen">
       <h1>Rose Coworking</h1>
       <p>Rose Coworking is a platform for finding and booking coworking spaces.</p>
       <p>Coming Soon</p>
